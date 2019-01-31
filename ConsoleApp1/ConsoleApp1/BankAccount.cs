bdbf332ecd8142792c4c8296d70da31b97903a80
Arif Hossain <arif.hossain177@gmail.com>
2019-01-31 10:50:34 +0600

    BankAccount Class Added

---
+++ ConsoleApp1/ConsoleApp1/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class BankAccount
+    {
+        public string AccountNo { get; set; }
+        public string AccountsName { get; set; }
+        public string AccountType { get; set; }
+    }
+}
